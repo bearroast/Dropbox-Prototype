8760cd5036d0f4ac41afeb7bc009db8ca49a9067
Bjørn Rostad <hi@bjornrostad.no>
2014-09-14 13:46:11 -0700

    login/sign up

---
+++ Dropbox Prototype/CreateAccountRootViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CreateAccountRootViewController.swift
+//  Dropbox Prototype
+//
+//  Created by Bjørn Eivind Rostad on 9/14/14.
+//  Copyright (c) 2014 Bjørn Eivind Rostad. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        performSegueWithIdentifier("performCreateAccount", sender: self)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
