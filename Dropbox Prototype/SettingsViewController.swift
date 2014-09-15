3b4e9e213839f89d4d77bed19a80dd4c7fe0cebd
Bjørn Rostad <hi@bjornrostad.no>
2014-09-15 09:04:55 -0700

    more work on SettingsViewController

---
+++ Dropbox Prototype/SettingsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsViewController.swift
+//  Dropbox Prototype
+//
+//  Created by Bjørn Eivind Rostad on 9/15/14.
+//  Copyright (c) 2014 Bjørn Eivind Rostad. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: 320, height: 1000)
+
+        scrollView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        // user scrolls
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        //
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        //
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        //
+    }
+
+}
