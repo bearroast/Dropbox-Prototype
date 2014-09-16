d4693c29ffa306be327af3e61b8a76aeafb85ff3
Bjørn Rostad <hi@bjornrostad.no>
2014-09-16 02:26:03 -0700

    adding swipe gestures to welcome

---
+++ Dropbox Prototype/WelcomeTwoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WelcomeTwoViewController.swift
+//  Dropbox Prototype
+//
+//  Created by Bjørn Eivind Rostad on 9/16/14.
+//  Copyright (c) 2014 Bjørn Eivind Rostad. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeTwoViewController: UIViewController {
+
+    @IBAction func goToNext(sender: UISwipeGestureRecognizer) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
